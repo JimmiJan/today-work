b213322e1ea7f63e2dedca0211ec187329082307
JimmiJan <96634490+JimmiJan@users.noreply.github.com>
2022-01-07 10:55:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,119 @@
+// var names = "Jamshed"
+// names = "Shahid"
+// console.log(names);
+
+
+// function foo(){
+//     var names = "jimmi"
+// //   console.log(names)
+// }
+
+
+// foo()
+// console.log(names)
+
+
+
+// let names = "james"
+// names = "jamshed"
+// console.log (names)
+
+
+
+// const names = "james"
+// const names = "jamshed"
+// console.log (names)
+
+
+
+// const names = "james"
+//  names = "jamshed"
+// console.log (names)
+
+
+
+
+
+// if (30 > 20) {
+//     console.log(true)
+//     const names = "jimmi"
+// } else {
+//     console.log(false)
+// }
+// console.log(names)
+
+
+
+
+// if (30 > 20) {
+//     console.log(true)
+
+//     const names = "jimmi"
+
+//     console.log(names)
+// } else {
+//     console.log(false)
+// }
+
+
+
+
+// if (30 > 20) {
+//     console.log(true)
+//     let names = "jimmi"
+// } else {
+//     console.log(false)
+// }
+// console.log(names)
+
+
+
+
+// if (30 > 20) {
+//     console.log(true)
+
+//     let names = "jimmi"
+
+//     console.log(names)
+// } else {
+//     console.log(false)
+// }
+
+
+
+
+
+
+// if (30 > 20) {
+//     console.log(true)
+//     var names = "jimmi"
+// } else {
+//     console.log(false)
+// }
+// console.log(names)
+
+
+
+
+// if (30 > 20) {
+//     console.log(true)
+
+//     var names = "jamshed"
+
+//     console.log(names)
+// } else {
+//     console.log(false)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
